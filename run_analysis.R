1a0c9d4b2deaadb7c4e9e3f8dea7101d59adc70f
Kelly <kelly@intrical.us>
2014-04-27 13:50:24 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,47 @@
-##read in data
-testX <- read.table("./X_test.txt")
-testY <- read.table("./y_test.txt")
-subjectTest <- read.table("./subject_test.txt")
-trainX <- read.table("./X_train.txt")
-trainY <- read.table("./y_train.txt")
-subjectTrain <- read.table("./subject_train.txt")
+#first, checks if UCI HAR Dataset folder exists in WD. If not, pulls
+# the zip file from the link and unzips it
+folder <- "UCI HAR Dataset"
+if(!file.exists(folder)) {
+  zipFile <- "src_data.zip"
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl, destfile=zipFile, method="curl")
+  unzip(zipFile)
+  file.remove(zipFile)
+}
 
-##create table with measurements
-xy <- rbind(testX, trainX)
+#check for required packages, install if necessary
+if (!require("reshape")) {
+  install.packages("reshape")
+}
 
-##calculate row means and sd, add to table
-xy$mean <- rowMeans(xy, na.rm = TRUE)
-xy$SD <- apply(xy,1, sd, na.rm = TRUE)
+if (!require("data.table")) {
+  install.packages("data.table")
+}
 
-#extract only means and SD in data frame
-meanSD <- subset(xy, select=c("mean", "SD"))
+#load required packages
+library("reshape")
+library("data.table")
+
+
+#read in data
+subjectTest <- read.table(paste(folder, "/test/subject_test.txt", sep=""))
+testY <- read.table(paste(folder, "/test/y_test.txt", sep=""))
+testX <- read.table(paste(folder, "/test/X_test.txt", sep=""))
+
+subjectTrain <- read.table(paste(folder, "/train/subject_train.txt", sep=""))
+trainY <- read.table(paste(folder, "/train/y_train.txt", sep=""))
+trainX <- read.table(paste(folder, "/train/X_train.txt", sep=""))
+
+variables <- read.table(paste(folder, "/features.txt", sep=""))
+varNames <- as.character(variables[,2])
+
 
 #create data frame for activities and subjects
 activities <- rbind(testY, trainY)
 subjects <- rbind(subjectTest, subjectTrain)
+
+##clean up R working space, delete items no longer needed
+rm(testY, trainY, subjectTest, subjectTrain, variables, fileUrl, folder, zipFile)
 
 #label activities
 activities[,1] <- gsub(1, "WALKING", as.matrix(activities[,1]))
@@ -28,6 +51,32 @@ activities[,1] <- gsub(4, "SITTING", as.matrix(activities[,1]))
 activities[,1] <- gsub(5, "STANDING", as.matrix(activities[,1]))
 activities[,1] <- gsub(6, "LAYING", as.matrix(activities[,1]))
 
-#merge subjects, activities, and means and SDs
-tidy <- cbind(subjects, activities, meanSD)
-names(tidy)[1:4] = c("subject", "activity", "mean", "SD")
+##create data frame for measurements
+measurements <- rbind(testX, trainX)
+names(measurements) <- varNames
+
+##clean up R working space, delete items no longer needed
+rm(testX, trainX, varNames)
+
+##extract rows with means or stds from measurements
+mesMeans <- measurements[ , grepl("mean", names(measurements)) ]
+mesMeans <- mesMeans[, -grep("Freq", names(mesMeans))]
+mesStds <- measurements[ , grepl("std", names(measurements)) ]
+mesSub <- cbind(mesMeans, mesStds)
+
+##clean up R working space, delete items no longer needed
+rm(mesMeans, mesStds)
+
+#merge subjects, activities, measurements
+data <- cbind(subjects, activities, mesSub)
+names(data)[1:2] = c("subject", "activity")
+
+##clean up R working space, delete items no longer needed
+rm(subjects, activities, measurements, mesSub)
+
+#melt dataset and find means by sub and act
+melted <- melt(data, id.vars = c("subject", "activity"))
+tidy <- cast(melted, subject + activity ~ variable, mean)
+
+##write the tidy data to a file
+write.table(tidy, file= "tidy.txt")
